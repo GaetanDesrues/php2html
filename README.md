64d3fa093acd5ef9c743f54f0b7a90e8d3a294ad
Desrues <gaetan.desrues@free.fr>
2020-02-25 10:38:27 +0100

    ok

--- README.md
+++ README.md
@@ -3,22 +3,18 @@
 ### Example
 
 ```
-$obj = new Elem('div', array('class'=>'class1', 'val'=>'a text'));
-$s1 = $obj->add('span', array('class'=>'class2'));
+$obj = new Elem('div', array('class'=>'class1', 'id'=>'text1'));
+$s1 = $obj->add('div', array('class'=>'class2'));
 $s1->add('span', array('val'=>'a span'));
-$obj->add('img', array('href'=>'source', 'id'=>'my_image', 'class'=>'my_class'));
-$obj->print();
+$obj->add('img', array('href'=>'source', 'id'=>'my_image', 'class'=>'my_class', 'close_tag'=>false));
+echo $obj->outerHtml();
 ```
 Output:
 ```
-<div class="class1">
-  a text
-  <span class="class2">
-    <span>
-      a span
-    </span>
-  </span>
-  <img class="my_class" id="my_image" href="source">
-  </img>
+<div class="class1" id="text1">
+ <div class="class2">
+  <span>a span</span>
+ </div>
+ <img href="source" id="my_image" class="my_class">
 </div>
 ```
--- libHtml.php
+++ libHtml.php
@@ -1,39 +1,53 @@
-
 <?php
 class Elem
 {
-  private $tree = array();
-  private $def = array('class'=>false, 'id'=>false, 'val'=>false, 'href'=>false, 'src'=>false);
+  private $tree; // Element root
 
-  public function __construct($elem, $args){
-    $args['elem'] = $elem;
-    $args = array_merge($this->def, $args);
+  public function __construct($elem, $args=array()){
+    $args['elem'] = $elem; // Element options
     $this->tree = array('args'=>$args, 'children'=>array());
   }
 
-  public function add($elem, $args)
-  {
-    array_push($this->tree['children'], new Elem($elem, $args));
-    return end($this->tree['children']);
+  public function add($elem, $args=array()){
+    array_push($this->tree['children'], new Elem($elem, $args)); // Push the new element to children array
+    return end($this->tree['children']); // New born element
   }
 
-  public function print($i=0)
+
+  public function outerHtml($i=0)
   {
-    $args = $this->tree['args'];
-    $sep = " ";
+    $args = $this->tree['args'];          // Retrieve element otions
+    $html = '';                           // Main html
 
+    $sep = " ";                           // Indent unit
+    $sp = str_repeat($sep,$i);            // Indent
+    $sp_val = (isset($args['val'])) ? '' : $sp; // Indent after inner html
+
+    $lb = "\n";                           // Line break unit
+    $lb_val = (isset($args['val'])) ? '' : $lb; // Line break around inner html
+
+    // Extract element type, val and extra_opt
+    $elem = $args['elem']; unset($args['elem']);
+    $val = (isset($args['val'])) ? $args['val'].$lb_val : ''; unset($args['val']);
+    $extra_opt = (isset($args['extra_opt'])) ? ' '.$args['extra_opt'] : ''; unset($args['extra_opt']);
+    $close_tag = (isset($args['close_tag'])) ? $args['close_tag'] : true; unset($args['close_tag']);
+
+
+    // Concat element tags
     $opt = '';
-    foreach (array('class','id','href','src') as $v) {
-      $opt .= ($args[$v]) ? ' '.$v.'="'.$args[$v].'"' : '';
+    foreach($args as $k => $v) {
+      $opt .= ' '.$k.'="'.$v.'"';
     }
-    $val = ($args['val']) ? str_repeat($sep,$i+2).$args['val']."\n" : '';
 
-    echo str_repeat($sep,$i).'<'.$args['elem'].$opt.">\n";
-      echo $val;
-      foreach ($this->tree['children'] as $value) {
-        $value->print($i+2);
+    // Write html output
+    $html .= $sp.'<'.$elem.$opt.$extra_opt.'>'.$lb_val;
+      $html .= $val;
+      foreach ($this->tree['children'] as $child) {
+        $html .= $child->outerHtml($i+1);
       }
-    echo str_repeat($sep,$i).'</'.$args['elem'].'>'."\n";
+    if($close_tag) $html .= $sp_val.'</'.$elem.'>'.$lb;
+
+    return $html;
   }
 }
 ?>
